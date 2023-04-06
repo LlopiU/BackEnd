180e09157ec5a33c1abe4838134bd2f783d5b048
LlopiU <llopiulisesalan10@gmail.com>
2023-04-06 18:44:30 -0300

    nuevo final

--- src/main/java/com/portfolio/ull/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ull/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200","https://frontend-ull-bdd20.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200","https://frontendull-3cead.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
